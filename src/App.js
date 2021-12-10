01db6ba18e13be3f4b43f0cf2097799d7cc0f89f
Zach Young <zach.young@kochcreativegroup.com>
2021-12-10 16:48:35 -0600

    text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import AWS from "aws-sdk";
 
 function App() {
   return (
     <div className="App">
       <h1>AWS POLLY</h1>
+      <input type="text" />
     </div>
   );
 }
