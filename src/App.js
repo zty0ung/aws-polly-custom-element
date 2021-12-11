987c20b33c4b452602d18f0a2912385724623b45
Zach Young <zach.young@kochcreativegroup.com>
2021-12-10 23:56:25 -0600

    text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
-
+import React, { useState } from "react";
 function App() {
+  const [transcript, setTranscript] = useState("");
+
+  const handleChange = (e) => {
+    setTranscript(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(transcript);
+  };
   return (
     <div className="App">
       <h1>AWS POLLY</h1>
-      <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Transcript:
+          <textarea type="text" value={transcript} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
